ca7b2146b1eb7d3e836fd49331153308f178d8ef
M-Faisal12 <mfaisalsaeed18@gmail.com>
2026-01-23 18:49:46 +0500

    BFS Corrected

--- main.cpp
+++ main.cpp
@@ -7,34 +7,26 @@ struct Tile
     int x, y;
     Tile(int ax=0, int ay=0, char sym='.') : x(ax), y(ay), symbol(sym) {}
 };
-int main()
+
+int* BFS(Tile* grid, int startX, int startY, int endX, int endY)
 {
-    Tile grid[100];
-    // Initialize grid with default tiles
-    grid[0] = Tile(0, 0, 'A'); // Example: Place an 'A' at position (0,0)
-    grid[99] = Tile(9, 9, 'B'); // Example: Place a 'B' at position (9,9)
-    for(int i=0; i<10; ++i)
-    {
-            for(int j=0; j<10; ++j)
-            {
-                grid[i*10+j] = Tile(i, j, '#');
-                cout<< grid[i*10+j].symbol << ' ';
-            }
-    cout<< endl;
-    }
+    //TODO BFS implementation would go here 
+    if(!grid || startX < 0 || startX >=10 || startY < 0 || startY >=10
+       || endX < 0 || endX >=10 || endY < 0 || endY >=10)
+        return nullptr;
     bool visited[100] = {false};
-    int parent[100];
+    int* parent=new int[100];
     for (int i = 0; i < 100; i++)
         parent[i] = -1;
     Queue<int> q;
-    q.enqueue(0);
-    visited[0] = true;
+    q.enqueue(startX + startY*10);
+    visited[startX + startY*10] = true;
     while(!q.isEmpty())
     {
-        if(grid[q.peek()].symbol == 'B')
+        if(grid[q.peek()].x == endX && grid[q.peek()].y == endY)
         {
             printf("Found B");
-            break;
+            return parent;
         }
         int i=grid[q.peek()].x,j=grid[q.peek()].y;
         q.dequeue();
@@ -103,4 +95,38 @@ int main()
             j++; i--;
         }
     }
+    return nullptr;
+}
+int main()
+{
+    // Tile grid[100];
+    Tile* grid=new Tile[100];
+    // Initialize grid with default tiles
+    for(int i=0; i<10; ++i)
+    {
+            for(int j=0; j<10; ++j)
+            {
+                grid[i*10+j] = Tile(i, j, '.');
+            }
+    cout<< endl;
+    }
+    grid[0] = Tile(0, 0, 'A'); // Example: Place an 'A' at position (0,0)
+    grid[48] = Tile(4, 8, 'B'); // Example: Place a 'B' at position (9,9)
+    
+    int* parent = BFS(grid, 0, 0, 4, 8);
+    int i=48;
+    while(parent[i] != -1)
+    {
+        grid[i].symbol = 'P';
+        i = parent[i];
+    }
+    cout<<endl;
+    for(int i=0; i<10; ++i)
+    {
+            for(int j=0; j<10; ++j)
+            {
+                cout << grid[i*10+j].symbol << ' ';
+            }
+            cout<< endl;
+        }
 }
